5d71b2038c64dc4acb6c45d792a4f8e6e36e114a
AbuFaishal3087 <abufaishal3087@gmail.com>
2026-01-16 15:30:56 +0530

    fixed responsive ui for rent new vehicle modal

--- src/components/dialogs/rent-new-vehicle/index.tsx
+++ src/components/dialogs/rent-new-vehicle/index.tsx
@@ -4,26 +4,37 @@ import RentVehicleForm from "@/forms/rent/RentVehicleForm";
 import { Car } from "lucide-react";
 import { cn } from "@/lib/utils";
 
-export default function RentNewVehicleDialog(
-  {
-    //   onClose,
-  }: DialogRegistry["RENT_VEHICLE"] & { onClose: () => void }
-) {
+export default function RentNewVehicleDialog({
+  onClose,
+}: DialogRegistry["RENT_VEHICLE"] & { onClose: () => void }) {
   return (
     <DialogContent
       className={cn(
         "w-full md:max-w-230 max-w-95vw rounded-md outline-none border-0 p-8",
-        "[&>button[data-slot='dialog-close']>svg]:w-6",
+        "md:[&>button[data-slot='dialog-close']>svg]:block",
+        //hiding default button for mobile screen
+        "[&>button[data-slot='dialog-close']>svg]:hidden",
         "[&>button[data-slot='dialog-close']>svg]:h-6",
         "[&>button[data-slot='dialog-close']]:mr-4",
         "[&>button[data-slot='dialog-close']]:mt-2"
       )}
     >
-      <div className="flex">
+      <div className="relative flex max-h-[100dvh] flex-col md:flex-row overflow-hidden">
+        {/* custom close button for mobile screen */}
+        <button
+          onClick={() => onClose()}
+          className="md:hidden absolute right-4 top-4 z-50 rounded-md p-2
+               bg-background/80 backdrop-blur
+               hover:bg-muted focus:outline-none
+               safe-top"
+        >
+          ✕
+        </button>
+
         {/* LEFT SECTION */}
-        <div className="w-[38%] bg-muted/40 px-8 py-10">
-          <DialogHeader className="text-left space-y-4">
-            <div className="flex h-14 w-14 items-center justify-center rounded-full bg-primary/10">
+        <div className="w-full md:w-[38%] bg-muted/40 px-4 pt-16 pb-6 md:px-8 md:pt-10 md:pb-10">
+          <DialogHeader className="space-y-4 text-center md:text-left">
+            <div className="mx-auto md:mx-0 flex h-14 w-14 items-center justify-center rounded-full bg-primary/10">
               <Car className="h-7 w-7 text-primary" />
             </div>
 
@@ -33,13 +44,12 @@ export default function RentNewVehicleDialog(
 
             <p className="text-sm leading-relaxed text-muted-foreground">
               Fill in the details below to rent a vehicle quickly and easily.
-              Please ensure all information is correct before submitting.
             </p>
           </DialogHeader>
         </div>
 
-        {/* RIGHT SECTION */}
-        <div className="flex-1 px-8 py-10">
+        {/* RIGHT SECTION (SCROLLABLE) */}
+        <div className="flex-1 overflow-y-auto px-4 py-6 md:px-8 md:py-10">
           <RentVehicleForm />
         </div>
       </div>
