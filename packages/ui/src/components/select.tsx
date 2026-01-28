f0ffbe726191bb10408deadf8f1b00ad65563949
AbuFaishal3087 <abufaishal3087@gmail.com>
2026-01-28 12:47:44 +0530

    removed commented code

--- packages/ui/src/components/select.tsx
+++ packages/ui/src/components/select.tsx
@@ -48,42 +48,6 @@ function SelectTrigger({
   );
 }
 
-// function SelectContent({
-//   className,
-//   children,
-//   position = "item-aligned",
-//   align = "center",
-//   ...props
-// }: React.ComponentProps<typeof SelectPrimitive.Content>) {
-//   return (
-//     <SelectPrimitive.Portal>
-//       <SelectPrimitive.Content
-//         data-slot="select-content"
-//         className={cn(
-//           "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 relative z-50 max-h-(--radix-select-content-available-height) min-w-[8rem] origin-(--radix-select-content-transform-origin) overflow-x-hidden overflow-y-auto rounded-md border shadow-md",
-//           position === "popper" &&
-//             "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
-//           className
-//         )}
-//         position={position}
-//         align={align}
-//         {...props}
-//       >
-//         <SelectScrollUpButton />
-//         <SelectPrimitive.Viewport
-//           className={cn(
-//             "p-1",
-//             position === "popper" &&
-//               "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)] scroll-my-1"
-//           )}
-//         >
-//           {children}
-//         </SelectPrimitive.Viewport>
-//         <SelectScrollDownButton />
-//       </SelectPrimitive.Content>
-//     </SelectPrimitive.Portal>
-//   )
-// }
 function SelectContent({
   className,
   children,
@@ -102,12 +66,6 @@ function SelectContent({
             "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
           className,
         )}
-        // className={cn(
-        //   "z-50 rounded-md border bg-white shadow-lg",
-        //   "w-[var(--radix-select-trigger-width)]",
-        //   "max-h-60 overflow-y-auto",
-        //   className,
-        // )}
         {...props}
       >
         <SelectPrimitive.Viewport className="p-1">
@@ -137,6 +95,7 @@ function SelectItem({
   return (
     <SelectPrimitive.Item
       {...props}
+      data-slot="select-item"
       className={cn(
         "focus:bg-accent focus:text-accent-foreground [&_svg:not([class*='text-'])]:text-muted-foreground relative flex w-full cursor-default items-center gap-2 rounded-sm px-4 py-2 text-sm select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 *:[span]:last:flex *:[span]:last:items-center *:[span]:last:gap-2",
         className,
